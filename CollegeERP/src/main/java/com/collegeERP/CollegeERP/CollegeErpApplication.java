1b2a55bd15787d030c9b9acd07e5b986c8b54f09
Mathivanan <mmathivanan17@gmail.com>
2019-11-11 21:40:10 +0530

    ERP Project

---
+++ CollegeERP/src/main/java/com/collegeERP/CollegeERP/CollegeErpApplication.java
@@ -0,0 +1,13 @@
+package com.collegeERP.CollegeERP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeErpApplication.class, args);
+	}
+
+}
---
+++ CollegeERP/src/test/java/com/collegeERP/CollegeERP/CollegeErpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.collegeERP.CollegeERP;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CollegeErpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
