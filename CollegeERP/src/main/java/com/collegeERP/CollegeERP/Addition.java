1a739ed8410dfe45067b9187459e4768d19e46c4
Mathivanan <mmathivanan17@gmail.com>
2019-11-11 21:51:12 +0530

    Addition.java

---
+++ CollegeERP/src/main/java/com/collegeERP/CollegeERP/Addition.java
@@ -0,0 +1,14 @@
+package com.collegeERP.CollegeERP;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int x = 1005;
+		int y = 582;
+		int z = x+y;
+		
+		System.out.println(z);
+
+	}
+
+}
